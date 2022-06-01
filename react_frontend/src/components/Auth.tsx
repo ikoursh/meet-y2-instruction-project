d8adf5b427820e8b0e459b8cf495ed6970b07736
Inbar Koursh <22639344+ikoursh@users.noreply.github.com>
2022-06-01 12:56:00 +0300

    tweak auth design

--- react_frontend/src/components/Auth.tsx
+++ react_frontend/src/components/Auth.tsx
@@ -5,16 +5,16 @@ import {Button, Form, TextInput, Grid, Checkbox, Link, PasswordInput} from 'carb
 //@ts-ignore
 import {ArrowRight} from '@carbon/icons-react';
 
-function isValidEmail(email:string){
-    return email.length>0&&email.includes("@");
+function isValidEmail(email: string) {
+    return email.length > 0 && email.includes("@");
 }
 
-export default function Auth(props: { AuthType:string }) {
+export default function Auth(props: { AuthType: string }) {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
     const [stage, setStage] = React.useState(0);
-    const isValid = !(stage==0?!isValidEmail(email):password.length<6);
-    const isLogin = props.AuthType=="login";
+    const isValid = !(stage == 0 ? !isValidEmail(email) : password.length < 6);
+    const isLogin = props.AuthType == "login";
     return (
         <div style={{position: "relative"}}>
 
@@ -22,35 +22,37 @@ export default function Auth(props: { AuthType:string }) {
             </img>
             <div className={"center"}>
 
-                <Form onSubmit={(e)=>{
+                <Form onSubmit={(e) => {
                     e.preventDefault();
                     if (isValidEmail(email)) {
                         setStage(1);
                     }
                 }}>
 
-                    <h2 className={"login"}>{isLogin?"Login":"Register"}</h2>
+                    <h2 className={"login"}>{isLogin ? "Login" : "Register"}</h2>
                     <div className={"help"}>
-                        {stage==0? <span>{isLogin?"Don't":"Already"} have and account?
-                                {isLogin?<Link href={"/register"}>Create one here</Link>:<Link href={"/login"}>Login here</Link>}</span>
-                            : <span>{isLogin?"Logging in":"Signing up"} as {email} <Link onClick={()=>setStage(0)}>Not you?</Link> </span>}
+                        {stage == 0 ? <span className={"help-text"}>{isLogin ? "Don't" : "Already"} {"have and account? "}
+                                {isLogin ? <Link href={"/register"}>Create one here</Link> :
+                                    <Link href={"/login"}>Login here</Link>}</span>
+                            :
+                            <span>{isLogin ? "Logging in" : "Signing up"} as {email} <Link onClick={() => setStage(0)}>Not you?</Link> </span>}
                     </div>
                     <div className={"divider"}/>
-                    {stage==0?<><
-                        TextInput
-                        id="email"
-                        invalidText="Invalid email."
-                        labelText="Email Address"
-                        placeholder="example@gmail.com"
-                        className={'text-input'}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
+                    {stage == 0 ? <><
+                            TextInput
+                            id="email"
+                            invalidText="Invalid email."
+                            labelText="Email Address"
+                            placeholder="example@gmail.com"
+                            className={'text-input'}
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                            }}
 
-                        value={email}
-                        invalid={!isValid && email.length>0}
-                    />
-                        <Checkbox id={"rememberId"} labelText={"Remember me"} className={"checkbox"}/></>:
+                            value={email}
+                            invalid={!isValid && email.length > 0}
+                        />
+                            <Checkbox id={"rememberId"} labelText={"Remember me"} className={"checkbox"}/></> :
                         <PasswordInput
                             id="password"
                             invalidText="Invalid password. Must have more then 6 characters."
@@ -62,9 +64,18 @@ export default function Auth(props: { AuthType:string }) {
                             }}
                             value={password}
                             type={'password'}
-                            invalid={!isValid && password.length>0}
+                            invalid={!isValid && password.length > 0}
                         />
                     }
+
+                    {stage == 1 ?
+                        <Button
+                            className={"button-back"}
+                            kind={"secondary"}
+                            onClick={() => {
+                                setStage(0);
+                            }}
+                        >Back</Button>:<></>}
 
                     <Button
                         kind="primary"
@@ -74,7 +85,7 @@ export default function Auth(props: { AuthType:string }) {
                         className={"button"}
                         disabled={!isValid}
                     >
-                        Continue
+                        {stage==0?"Continue":(isLogin?"Login":"Register")}
                     </Button>
                 </Form>
 
